d410147ff4c80d001c5f1acc30422e5747521c61
Argo Roots <argo@roots.ee>
2019-04-09 21:26:59 +0300

    Create lambda.js

---
+++ lambda.js
@@ -0,0 +1,52 @@
+const http = require('https')
+
+
+const getDiscogsJson = async () => {
+  return new Promise((resolve, reject) => {
+    const options = {
+      host: 'api.discogs.com',
+      port: 443,
+      path: `/users/${process.env.DISCOGS_USER}/collection/folders/0/releases?per_page=50&sort=artist&token=${process.env.DISCOGS_TOKEN}`,
+      method: 'GET',
+      headers: { 'User-Agent': 'AWS Lambda' }
+    }
+    http.get(options, response => {
+      var body = ''
+
+      response.on('data', function(d) {
+        body += d
+      })
+
+      response.on('end', function() {
+        var parsed = JSON.parse(body)
+        resolve(parsed.releases)
+      })
+    })
+  })
+}
+
+
+exports.handler = async (event) => {
+  const posts = await getDiscogsJson()
+  const result = posts.map(p => {
+    return {
+      id: p.id,
+      title: p.basic_information.title,
+      year: p.basic_information.year,
+      artist: p.basic_information.artists[0].name,
+      picture: p.basic_information.cover_image,
+      url: p.basic_information.resource_url
+    }
+  })
+
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Cache-Control': 'max-age=43200, must-revalidate'
+    },
+    body: JSON.stringify(result)
+  }
+
+  return response
+}
